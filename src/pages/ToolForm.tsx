442517940781e64d16c73b5bb35d75f12ae23231
Nan <llanos.adrian@hotmail.com>
2025-07-11 12:06:23 -0600

    feat: cambie el endpoint para el bot, para que busque la informacion limitada

--- src/pages/ToolForm.tsx
+++ src/pages/ToolForm.tsx
@@ -311,7 +311,11 @@ const ToolForm: React.FC = () => {
     if (selectedTable && envValue && user) {
       const envOpt = ENV_OPTIONS.find(opt => opt.value === envValue);
       if (envOpt && envOpt.baseUrl) {
+<<<<<<< Updated upstream
         setValue('config.endpoint', `${envOpt.baseUrl}/records/table/${user.c_name}/${selectedTable.slug}`);
+=======
+        setValue('config.endpoint', `${envOpt.baseUrl}/records/bot/table/${user.companySlug}/${selectedTable.slug}`);
+>>>>>>> Stashed changes
       }
     }
   }, [watch('config.environment'), selectedTable, user, setValue]);
