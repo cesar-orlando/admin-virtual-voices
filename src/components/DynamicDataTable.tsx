9bb9f2e65255e28f1614db1939a3d4dc063a5225
CesarLopezF <cesar.lopez42@outlook.com>
2025-07-21 10:14:56 -0600

    chore: ordenar records de dynamic table por updatedAt

--- src/components/DynamicDataTable.tsx
+++ src/components/DynamicDataTable.tsx
@@ -161,7 +161,7 @@ export default function DynamicDataTable({
         user, 
         page + 1, 
         rowsPerPage,
-        'createdAt',
+        'updatedAt',
         'desc',
         activeFilters
       );
